a685ee0a8094b3ed64e073b406b5f41ffdb77525
horiamalancus <76810455+horiamalancus@users.noreply.github.com>
2021-04-07 11:29:49 +0300

    Add files via upload

--- continut/js/script.js
+++ continut/js/script.js
@@ -1,9 +1,19 @@
-function data()
-{
-    let d = new Date();
-    let URL = window.location.href;
-    let locatie = window.location;
+function data() {
+  let d = new Date();
+  let URL = window.location.href;
+  let locatie = window.location;
 
-    document.getElementById("i1").innerHTML="Data curenta este:"+d;
-    document.getElementById("i2").innerHTML="URL-ul paginii este:"+URL;
+  document.getElementById("i1").innerHTML = "Data curenta este:" + d;
+  document.getElementById("i2").innerHTML = "URL-ul paginii este:" + URL;
+}
+
+function schimbaContinut(resursa) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("continut").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", `${resursa}.html`, true);
+  xhttp.send();
 }
